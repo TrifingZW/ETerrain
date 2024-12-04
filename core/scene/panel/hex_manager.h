98cfef72f8bb92ae8313ffde862c9c77b8b27982
TrifingZW <67575386+TrifingZW@users.noreply.github.com>
2024-12-05 02:58:49 +0800

    修复一个错误

--- core/scene/panel/hex_manager.h
+++ core/scene/panel/hex_manager.h
@@ -14,7 +14,7 @@ namespace HexMetrics
     constexpr float TileHeight = 129.0f;
     constexpr float SideLength = 76.0f;
     constexpr float OuterRadius = SideLength;
-    constexpr float InnerRadius = SideLength * (sqrt(3.0f) / 2.0f);
+    constexpr float InnerRadius = SideLength * 0.866025404f;
     constexpr glm::vec3 corners[] = {
         glm::vec3(0.0f, 0.0f, OuterRadius),
         glm::vec3(InnerRadius, 0.0f, 0.5f * OuterRadius),
