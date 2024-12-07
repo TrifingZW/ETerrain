d5090c20acbc1beb13ef03c2efe3557222d6be93
TrifingZW <67575386+TrifingZW@users.noreply.github.com>
2024-12-07 17:59:42 +0800

    完善 Hex 六边形坐标托管器

--- core/scene/panel/hex_manager.h
+++ core/scene/panel/hex_manager.h
@@ -5,6 +5,7 @@
 #pragma once
 #include <cmath>
 
+#include "glm/vec2.hpp"
 #include "glm/vec3.hpp"
 #include "glm/detail/func_packing_simd.inl"
 
@@ -12,10 +13,11 @@ namespace HexMetrics
 {
     constexpr float TileWidth = 148.0f;
     constexpr float TileHeight = 129.0f;
-    constexpr float SideLength = 76.0f;
+
+    constexpr float SideLength = 1.0f;
     constexpr float OuterRadius = SideLength;
     constexpr float InnerRadius = SideLength * 0.866025404f;
-    constexpr glm::vec3 corners[] = {
+    /*constexpr glm::vec3 Corners[] = {
         glm::vec3(0.0f, 0.0f, OuterRadius),
         glm::vec3(InnerRadius, 0.0f, 0.5f * OuterRadius),
         glm::vec3(InnerRadius, 0.0f, -0.5f * OuterRadius),
@@ -23,14 +25,27 @@ namespace HexMetrics
         glm::vec3(-InnerRadius, 0.0f, -0.5f * OuterRadius),
         glm::vec3(-InnerRadius, 0.0f, 0.5f * OuterRadius),
         glm::vec3(0.0f, 0.0f, OuterRadius)
+    };*/
+    constexpr glm::vec3 Corners[] = {
+        glm::vec3(OuterRadius, 0.0f, 0.0f),
+        glm::vec3(OuterRadius * 0.5f, 0.0f, -InnerRadius),
+        glm::vec3(-OuterRadius * 0.5f, 0.0f, -InnerRadius),
+        glm::vec3(-OuterRadius, 0.0f, 0.0f),
+        glm::vec3(-OuterRadius * 0.5f, 0.0f, InnerRadius),
+        glm::vec3(OuterRadius * 0.5f, 0.0f, InnerRadius),
     };
 }
 
 class HexManager
 {
 public:
-    int gridWidth, gridHeight;
-    float pixelWidth, pixelHeight;
+    float hexWidth, hexHeight;
+    int tileWidth, tileHeight, sideLength;
+    float outerRadius, innerRadius;
+    HexManager(float hexWidth, float hexHeight, int tileWidth, int tileHeight);
 
-    HexManager(int gridWidth, int gridHeight);
+    [[nodiscard]] glm::vec2 HexToPixel(const glm::ivec2& hex) const;
+    [[nodiscard]] glm::ivec2 PixelToHex(const glm::vec2& pixel) const;
+    [[nodiscard]] float GetPixelWidth() const;
+    [[nodiscard]] float GetPixelHeight() const;
 };
